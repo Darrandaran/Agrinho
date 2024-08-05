9f7617d166a7c65e48d1bd44f82586fa15712b39
Darrandaran <132006096+Darrandaran@users.noreply.github.com>
2024-08-04 22:48:42 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,25 @@
+
+window.aparecer = ScrollReveal({reset:true})
+
+aparecer.reveal('.sobe',{
+    duration: 2000,
+    distance:'90px'
+})
+//titulo
+aparecer.reveal('.h2',{
+    duration: 2000,
+    distance:'90px',
+})
+
+aparecer.reveal('.p',{
+    duration: 2000,
+    distance:'90px',
+    delay: 500
+})
+
+aparecer.reveal('.ha',{
+    duration: 2000,
+    distance:'90px',
+    delay: 250,
+    origin:'left'
+})
