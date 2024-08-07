636b9afe23202ac785fbeed696fec02a3b8146f7
Darrandaran <132006096+Darrandaran@users.noreply.github.com>
2024-08-07 10:39:18 -0300

    Update reveal.js

--- reveal.js
+++ reveal.js
@@ -7,18 +7,18 @@ aparecer.reveal('.sobe',{
 })
 //titulo
 aparecer.reveal('.h2',{
-    duration: 2000,
+    duration: 1000,
     distance:'90px',
 })
 
 aparecer.reveal('.p',{
-    duration: 2000,
+    duration: 1500,
     distance:'90px',
     delay: 500
 })
 
 aparecer.reveal('.ha',{
-    duration: 2000,
+    duration: 1000,
     distance:'90px',
     delay: 250,
     origin:'left'
